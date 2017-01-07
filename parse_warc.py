15538ceda85a5d3f05e0392cea5049af8c076972
Nick <nmapsy@gmail.com>
2017-01-07 13:31:03 -0800

    parse_warc.py: Parse the WARCs into tweet JSONS.

---
+++ parse_warc.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import unicode_literals
+import sys
+import json
+import logging
+import argparse
+
+
+def parse_warc(warc_path):
+  tweet_json = ''
+  header = False
+  with open(warc_path, 'rU') as warc:
+    for line in warc:
+      if header:
+        if line.startswith('Content-Length:'):
+          header = False
+        continue
+      else:
+        if line == 'WARC/1.0\n':
+          header = True
+          if tweet_json:
+            tweet = json.loads(tweet_json)
+            yield tweet
+          tweet_json = ''
+          continue
+      tweet_json += line
+
+def main(argv):
+  for tweet in parse_warc(argv[1]):
+    print(tweet)
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv))
