f2caba7b8c77e9e71c2cd7d8401864958051aa33
Nick <nmapsy@gmail.com>
2017-01-15 15:59:48 -0800

    crawl.py: Re-retrieve the original tweets too, since they're often truncated.

--- crawl.py
+++ crawl.py
@@ -12,8 +12,9 @@ import parse_warc
 
 KEY_NAMES = ('consumer_key', 'consumer_secret', 'access_token_key', 'access_token_secret')
 ARG_DEFAULTS = {'log':sys.stderr, 'volume':logging.WARNING}
-DESCRIPTION = """This script will read a series of tweets, then crawl Twitter to gather replies and
-other information related to them."""
+DESCRIPTION = """This script will read a series of tweets from unzipped WARC files, then use the
+Twitter API to re-retrieve them (to get the full, un-truncated text) and gather replies and other
+information related to them."""
 EPILOG = """Requires the python-twitter module in order to interact with Twitter:
 https://pypi.python.org/pypi/python-twitter/"""
 
@@ -48,32 +49,29 @@ def main(argv):
   logging.basicConfig(stream=args.log, level=args.volume, format='%(message)s')
   tone_down_logger()
 
-  if args.limit is None:
+  if args.parse_tweets or args.limit is None:
     remaining = sys.maxsize
   else:
     remaining = args.limit
 
-  keys = {}
-  if args.oauth_file:
-    keys = read_oauth_config(args.oauth_file, KEY_NAMES)
-  else:
-    for key_name in key_names:
-      key = getattr(args, key_name)
-      if key:
-        keys[key_name] = key
-      else:
-        keys = {}
-        break
-
-  if keys:
+  # Initialize the Twitter API object.
+  if not args.parse_tweets:
+    if args.oauth_file:
+      keys = read_oauth_config(args.oauth_file, KEY_NAMES)
+    else:
+      keys = {}
+      for key_name in key_names:
+        key = getattr(args, key_name)
+        if key:
+          keys[key_name] = key
+        else:
+          fail('All four OAuth tokens must be given unless --parse-tweets is.')
     try:
       import twitter
     except ImportError:
       fail('Interacting with Twitter requires the python-twitter module: '
            'https://pypi.python.org/pypi/python-twitter/')
     api = twitter.Api(tweet_mode='extended', sleep_on_rate_limit=True, **keys)
-  elif not args.parse_tweets:
-    fail('OAuth keys must be given if --parse-tweets isn\'t.')
 
   empties = 0
   entry_num = 0
@@ -86,23 +84,30 @@ def main(argv):
         logging.debug(json_pretty_format(entry))
         continue
       if args.parse_tweets:
-        print('https://twitter.com/{}/status/{}'.format(tweet['screen_name'], tweet['id']))
+        print('https://twitter.com/{screen_name}/status/{id}'.format(**tweet))
         print(tweet['text'])
-        if keys:
-          reply_chain = get_replied_tweets(tweet['in_reply_to_status_id'], api, remaining=remaining)
-          remaining -= len(reply_chain)
-          if tweet['in_reply_to_status_id']:
-            logging.info('Reply tweet; retrieved {} in replied-to chain.'.format(len(reply_chain)))
-          for reply in reply_chain:
-            # Note: 'full_text' is needed instead of 'text' in order to get new-style tweets over
-            # 140 characters, including @mentions and links:
-            # https://dev.twitter.com/overview/api/upcoming-changes-to-tweets
-            print('Replied: https://twitter.com/{}/status/{}'.format(reply.user.screen_name, reply.id))
-            print(reply.full_text.encode('utf-8'))
+        if tweet['in_reply_to_status_id']:
+          print('A reply to: https//twitter.com/{in_reply_to_screen_name}/status/'
+                '{in_reply_to_status_id}'.format(**tweet))
         print()
-      if keys and remaining <= 0:
+      else:
+        # Use the Twitter API to re-retrieve this tweet (to get the full text), and the full reply
+        # chain if it was a reply.
+        #TODO: Check if it's actually truncated, and if not, just use the original tweet data.
+        reply_chain = get_replied_tweets(tweet['id'], api, remaining=remaining)
+        remaining -= len(reply_chain)
+        if tweet['in_reply_to_status_id']:
+          logging.info('Reply tweet; retrieved {} in reply chain.'.format(len(reply_chain)))
+        for reply in reply_chain:
+          # Note: 'full_text' is needed instead of 'text' in order to get new-style tweets over
+          # 140 characters, including @mentions and links:
+          # https://dev.twitter.com/overview/api/upcoming-changes-to-tweets
+          print('https://twitter.com/{}/status/{}'.format(reply.user.screen_name, reply.id))
+          print(reply.full_text.encode('utf-8'))
+        print()
+      if remaining <= 0:
         break
-    if keys and remaining <= 0:
+    if remaining <= 0:
       break
   logging.info('Empties: {}'.format(empties))
 
